d7041c69826db87831653a4419abc0942c05b263
Hurvashi Dewangan <hurvashidewangan8118@gmail.com>
2023-12-13 12:58:12 +0530

    added sections

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+// const navLinks = document.querySelectorAll('.navbar-items a');
+
+
+// navLinks.forEach(link => {
+//     link.addEventListener('mouseenter', () => {
+//         link.style.color = 'blue'; // Change text color on hover
+//         link.style.textDecoration = 'underline'; // Add underline on hover
+//     });
+
+//     link.addEventListener('mouseleave', () => {
+//         link.style.color = 'black'; // Revert text color on mouse leave
+//         link.style.textDecoration = 'none'; // Remove underline on mouse leave
+//     });
+// });
+
